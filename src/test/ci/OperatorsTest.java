2a2f632ac6f57260aa56a7eb17ffaa993c769ba4
jasondai1991 <jasondai@jasondaidembp.gateway.sonic.net>
2018-04-12 23:51:35 -0700

    test6

--- src/test/ci/OperatorsTest.java
+++ src/test/ci/OperatorsTest.java
@@ -21,5 +21,29 @@ public class OperatorsTest {
         assertEquals(3.3,Operators.add(a,b),delta);
     }
 
+    //Test Minus
+    @Test
+    public void oneNumMinusZeroEqualsItself(){
+        double a=2.5;
+        double b = 0;
+        assertEquals(2.5,Operators.minus(a,b),delta);
+    }
+
+    @Test
+    public void oneNumMinusItselfEqualsZero(){
+        double a = 2.5;
+        double b = 2.5;
+        assertEquals(0,Operators.minus(a,b),delta);
+    }
+
+    @Test
+    public void testRegularMinusOperator(){
+        double a=2.3;
+        double b=2.6;
+        double c=-0.3;
+        assertEquals(c,Operators.minus(a,b),delta);
+    }
+
+
 
 }
