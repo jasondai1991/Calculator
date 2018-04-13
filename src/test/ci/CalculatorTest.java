2e0bcaa7a7bb527a0d6083537bf8c4dbb4bc2da1
jasondai1991 <jasondai@jasondaidembp.gateway.sonic.net>
2018-04-12 22:13:31 -0700

    new

--- src/test/ci/CalculatorTest.java
+++ src/test/ci/CalculatorTest.java
@@ -4,11 +4,16 @@ import org.junit.Test;
 
 //import static com.sun.org.apache.xerces.internal.util.PropertyState.is;
 import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 public class CalculatorTest {
 
     private Calculator interestCalculator;
+
+    @Test
+    public void emptyTest(){
+        assertEquals("1","1");
+    }
 
 
 }
