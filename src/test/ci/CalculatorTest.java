34120a1833313dbf258ddff03caf357dbc7486a3
jasondai1991 <jasondai@jasondaidembp.gateway.sonic.net>
2018-04-12 21:57:58 -0700

    change name

---
+++ src/test/ci/CalculatorTest.java
@@ -0,0 +1,14 @@
+package ci;
+import org.junit.Before;
+import org.junit.Test;
+
+//import static com.sun.org.apache.xerces.internal.util.PropertyState.is;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+public class CalculatorTest {
+
+    private Calculator interestCalculator;
+
+
+}
