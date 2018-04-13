3907a9a07357b8ed88a2c8ccf57f25d8b64f6b35
jasondai1991 <jasondai@jasondaidembp.gateway.sonic.net>
2018-04-13 01:38:29 -0700

    test4

--- src/main/ci/InputValidator.java
+++ src/main/ci/InputValidator.java
@@ -3,28 +3,44 @@ import java.util.*;
 
 public class InputValidator {
     private static String Operators = "+-*/";
+    private static String nums = "1234567890";
     private static String validChars = "+-*/()1234567890.";
 
 
-    public static boolean validate(String input) {
-        input = input.trim();
+    public boolean validate(String input) {
+        String[] segs = input.split("\\s+");
+        input = String.join("",segs);
         if (input.equals("")) return false;
-        boolean hasNumber = false;
         Stack<Character> parentesis = new Stack<Character>();
-        for (int i = 0; i < input.length(); i++) {
+        for (int i = 0; i < input.length()-1; i++) {
             char cur = input.charAt(i);
             if(validChars.indexOf(cur)==-1) return false;
             if (cur == '(') {
+                if(!validateForLeftParOrOps(input.charAt(i+1))) return false;
                 parentesis.push(cur);
                 continue;
             }
-            if (cur == ')') {
+            else if (cur == ')') {
                 if (parentesis.isEmpty()) return false;
+                if(!validateForRightPar(input.charAt(i+1))) return false;
                 parentesis.pop();
                 continue;
             }
+            else if(Operators.indexOf(cur)!=-1){
+                if(validateForLeftParOrOps(input.charAt(i+1))) continue;
+                return false;
+            }
         }
-        return parentesis.size()==0;
+        if(input.charAt(input.length()-1)==')') return parentesis.size()==1;
+        else if(nums.indexOf(input.charAt(input.length()-1))!=-1) return parentesis.size()==0;
+        else return false;
+    }
 
+    private boolean validateForLeftParOrOps(char c){
+        return !(nums.indexOf(c)==-1 && c!='(');
+    }
+
+    private boolean validateForRightPar(char c){
+        return !(nums.indexOf(c)!=-1 || c=='.' || c=='(');
     }
 }
--- src/test/ci/InputValidatorTest.java
+++ src/test/ci/InputValidatorTest.java
@@ -5,27 +5,80 @@ import org.junit.Test;
 
 public class InputValidatorTest {
 
+    InputValidator iv = new InputValidator();
+
     @Test
     public void emptyInputIsNotvalid(){
         String input ="";
-        assertFalse(InputValidator.validate(input));
+        assertFalse(iv.validate(input));
     }
 
     @Test
     public void diffrentNumOfLeftAndRightParenthesisIsNotValid(){
         String RightMore = "(1+2)*3)";
         String LeftMore = "(((1+2*3)";
-        assertFalse(InputValidator.validate(RightMore));
-        assertFalse(InputValidator.validate(LeftMore));
+        assertFalse(iv.validate(RightMore));
+        assertFalse(iv.validate(LeftMore));
     }
 
     @Test
     public void expressionContainsCharsOtherThanParenthesisNumAndOperatorIsNotValid(){
         String express = "(1+2*m+3)*5";
         String express2 = "(2+3'5*4)";
-        assertFalse(InputValidator.validate(express));
-        assertFalse(InputValidator.validate(express2));
+        assertFalse(iv.validate(express));
+        assertFalse(iv.validate(express2));
     }
+
+    @Test
+    public void expressionWithSpacesAreValid(){
+        String express = " 1+   3*  (5*6)";
+        assertTrue(iv.validate(express));
+    }
+
+
+    //Corner cases: two dots; two operators; operator and dot; operator and parenthesis; dot and parenthesis
+
+    @Test
+    public void leftParFollowedByNonNumOrLeftParIsNotValid(){
+        String express1 = "(1+2*()+5)*10";
+        String express2 = "(*1+2)*5";
+        String express3 = "(.1+2)*3";
+        assertFalse(iv.validate(express1));
+        assertFalse(iv.validate(express2));
+        assertFalse(iv.validate(express3));
+    }
+
+    @Test
+    public void rightParFollowedByNumOrLeftParOrDotIsNotValid(){
+        String express1 = "(1+2*)(+5)*10";
+        String express2 = "(*1+2).5";
+        String express3 = "(.1+2)3+5";
+        assertFalse(iv.validate(express1));
+        assertFalse(iv.validate(express2));
+        assertFalse(iv.validate(express3));
+    }
+
+    @Test
+    public void operatorShouldBeFollowedByNumOrLeftPar(){
+        String express1 = "1+2*5";
+        String express2 = "1+2*(3+6)";
+        String express3 = "1+.2";
+        String express4 = "1++2";
+        String express5 = "1*)";
+        assertTrue(iv.validate(express1));
+        assertTrue(iv.validate(express2));
+        assertFalse(iv.validate(express3));
+        assertFalse(iv.validate(express4));
+        assertFalse(iv.validate(express5));
+    }
+
+    /*public void OperatorAndDotNotSurroundedByNumsAreNotValid(){
+        String twoOperatorsTogetherInput = "(1+2*3/*4)*5";
+        assertFalse(iv.validate(twoOperatorsTogetherInput));
+    }*/
+
+
+
 
 
 
