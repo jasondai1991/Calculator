2a05919639452961fbf02c32c8eaf26efc03bf91
jasondai1991 <jasondai@jasondaidembp.gateway.sonic.net>
2018-04-13 01:57:04 -0700

    test 5

--- src/main/ci/InputValidator.java
+++ src/main/ci/InputValidator.java
@@ -12,23 +12,37 @@ public class InputValidator {
         input = String.join("",segs);
         if (input.equals("")) return false;
         Stack<Character> parentesis = new Stack<Character>();
+        boolean prevDot =false;
+        if(!validateForLeftParOrOps(input.charAt(0))) return false;
         for (int i = 0; i < input.length()-1; i++) {
             char cur = input.charAt(i);
             if(validChars.indexOf(cur)==-1) return false;
             if (cur == '(') {
                 if(!validateForLeftParOrOps(input.charAt(i+1))) return false;
                 parentesis.push(cur);
+                prevDot =false;
                 continue;
             }
             else if (cur == ')') {
                 if (parentesis.isEmpty()) return false;
                 if(!validateForRightPar(input.charAt(i+1))) return false;
                 parentesis.pop();
+                prevDot = false;
                 continue;
             }
             else if(Operators.indexOf(cur)!=-1){
-                if(validateForLeftParOrOps(input.charAt(i+1))) continue;
-                return false;
+                if(!validateForLeftParOrOps(input.charAt(i+1))) return false;
+                prevDot = false;
+                continue;
+            }
+            else if(cur=='.'){
+                if(nums.indexOf(input.charAt(i+1))==-1 || prevDot) return false;
+                prevDot = true;
+                continue;
+            }
+            else{
+                if(input.charAt(i+1)=='(') return false;
+                continue;
             }
         }
         if(input.charAt(input.length()-1)==')') return parentesis.size()==1;
--- src/test/ci/InputValidatorTest.java
+++ src/test/ci/InputValidatorTest.java
@@ -72,14 +72,53 @@ public class InputValidatorTest {
         assertFalse(iv.validate(express5));
     }
 
-    /*public void OperatorAndDotNotSurroundedByNumsAreNotValid(){
-        String twoOperatorsTogetherInput = "(1+2*3/*4)*5";
-        assertFalse(iv.validate(twoOperatorsTogetherInput));
-    }*/
+    @Test
+    public void dotOnlyFollowedByNumIsValid(){
+        String exp1 = "1.1+5";
+        String exp2 = "1.+";
+        String exp3 = "1.(3+4)";
+        String exp4 = "1..3+4";
+        String exp5 = "1.)(*3";
+        String exp6 = "3.5.5*6";
+        assertTrue(iv.validate(exp1));
+        assertFalse(iv.validate(exp2));
+        assertFalse(iv.validate(exp3));
+        assertFalse(iv.validate(exp4));
+        assertFalse(iv.validate(exp5));
+        assertFalse(iv.validate(exp6));
+    }
 
+    @Test
+    public void numShouldNotBeFollowedByLeftPar(){
+        String exp = "19(2+3)";
+        assertFalse(iv.validate(exp));
+    }
 
+    @Test
+    public void expressionShouldStartWithNumOrLeftPar(){
+        String exp1 = ".3+3";
+        String exp2 = ")*3(0";
+        String exp3 = "+5*10";
+        assertFalse(iv.validate(exp1));
+        assertFalse(iv.validate(exp2));
+        assertFalse(iv.validate(exp3));
+    }
 
+    @Test
+    public void expressionOnlyEndsWithNumOrRightPar(){
+        String exp1 = "3+3.";
+        String exp2 = "30*5(";
+        String exp3 = "3.3*5+";
+        String exp4 = "33*5()";
+        assertFalse(iv.validate(exp1));
+        assertFalse(iv.validate(exp2));
+        assertFalse(iv.validate(exp3));
+        assertFalse(iv.validate(exp4));
+    }
 
-
-
+    @Test
+    public void aRegularOneShouldBeValid(){
+        String express = "((3*1.5)/(10-3*(3.3-4.4*5)))*10-5.32";
+        assertTrue(iv.validate(express));
+    }
 }
