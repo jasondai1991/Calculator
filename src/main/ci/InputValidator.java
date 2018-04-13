5b92dcc74549b56997ac0501b4095e9ee8f1848c
jasondai1991 <jasondai@jasondaidembp.gateway.sonic.net>
2018-04-13 00:34:04 -0700

    test1

--- src/main/ci/InputValidator.java
+++ src/main/ci/InputValidator.java
@@ -8,29 +8,6 @@ public class InputValidator {
     public static boolean validate(String input) {
         input = input.trim();
         if (input.equals("")) return false;
-        boolean lastSymbleOperator = false;
-        boolean hasNumber = false;
-        Stack<Character> parentesis = new Stack<Character>();
-        for (int i = 0; i < input.length(); i++) {
-            char cur = input.charAt(i);
-            if (cur == ' ') continue;
-            if (Operators.indexOf(cur) != -1) {
-                if (lastSymbleOperator) return false;
-                lastSymbleOperator = true;
-                continue;
-            }
-            if (cur == '(') {
-                parentesis.push(cur);
-                continue;
-            }
-            if (cur == ')') {
-                if (parentesis.isEmpty()) return false;
-                parentesis.pop();
-                continue;
-            }
-            lastSymbleOperator = false;
-            hasNumber = true;
-        }
-        return hasNumber && parentesis.size()==0;
+        return true;
     }
 }
--- src/test/ci/InputValidatorTest.java
+++ src/test/ci/InputValidatorTest.java
@@ -11,28 +11,4 @@ public class InputValidatorTest {
         assertFalse(InputValidator.validate(input));
     }
 
-    @Test
-    public void diffrentLeftAndRightParenthesisIsNotValid(){
-        String RightMore = "(1+2)*3)";
-        String LeftMore = "(((1+2*3)";
-        assertFalse(InputValidator.validate(RightMore));
-        assertFalse(InputValidator.validate(LeftMore));
-    }
-
-    @Test
-    public void twoOperatorsTogetherIsNotValid(){
-        String twoOperatorsTogetherInput = "(1+2*3/*4)*5";
-        assertFalse(InputValidator.validate(twoOperatorsTogetherInput));
-    }
-
-    @Test
-    public void aRegularMathExpressionIsValid(){
-        String input1 = "1+2*3";
-        String input2 = "(1+2)*3*5/3";
-        String input3 = "((1*2)/3+5)*10";
-        assertTrue(InputValidator.validate(input1));
-        assertTrue(InputValidator.validate(input2));
-        assertTrue(InputValidator.validate(input3));
-    }
-
 }
