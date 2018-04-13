99fe9820118a8754d7d812c4f4fe47a2333f89a2
jasondai1991 <jasondai@jasondaidembp.gateway.sonic.net>
2018-04-12 23:02:30 -0700

    test1

--- src/test/ci/InputTest.java
+++ src/test/ci/InputTest.java
@@ -7,15 +7,10 @@ import static org.junit.Assert.*;
 public class InputTest {
     @Test
     public void emptyInputIsInvalid(){
-        Calculator cl = new Calculator();
         String input ="";
         assertFalse(Input.validate(input));
     }
 
-    /*@Test
-    public void LeftAndRightParenthesesShouldBeSameNumber(){
-
-    }*/
 
 
 
