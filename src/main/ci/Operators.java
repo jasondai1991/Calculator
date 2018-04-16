bf235972c51c36d265a581d45a998b4d28027d7a
jasondai1991 <jasondai@jasondaidembp.gateway.sonic.net>
2018-04-15 22:09:26 -0700

    version8

---
+++ src/main/ci/Operators.java
@@ -0,0 +1,14 @@
+package ci;
+
+public class Operators {
+
+    public static double add(double a,double b){
+        return a+b;
+    }
+
+    public static double minus(double a, double b){
+        return a-b;
+    }
+
+
+}
---
+++ src/test/ci/OperatorsTest.java
@@ -0,0 +1,49 @@
+package ci;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class OperatorsTest {
+
+    double delta = 0.000001;
+
+    //Test Add
+    @Test
+    public void oneNumAddZeroEqualsItself(){
+        double a=3.2;
+        double b=0;
+        assertEquals(3.2,3.2,delta);
+    }
+
+    @Test
+    public void testRegularAddOperator(){
+        double a = 1;
+        double b =2.3;
+        assertEquals(3.3,Operators.add(a,b),delta);
+    }
+
+    //Test Minus
+    @Test
+    public void oneNumMinusZeroEqualsItself(){
+        double a=2.5;
+        double b = 0;
+        assertEquals(2.5,Operators.minus(a,b),delta);
+    }
+
+    @Test
+    public void oneNumMinusItselfEqualsZero(){
+        double a = 2.5;
+        double b = 2.5;
+        assertEquals(0,Operators.minus(a,b),delta);
+    }
+
+    @Test
+    public void testRegularMinusOperator(){
+        double a=2.3;
+        double b=2.6;
+        double c=-0.3;
+        assertEquals(c,Operators.minus(a,b),delta);
+    }
+
+
+}
